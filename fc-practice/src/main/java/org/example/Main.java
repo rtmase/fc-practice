5586cb3319bd51f7cf03adc07a3585ea4db44bd8
rtmase <rtmase@gmail.com>
2023-05-15 17:19:50 +0900

    Update #3 - Main
    변수 클래스를 인터페이스로 변경

--- fc-practice/src/main/java/org/example/Main.java
+++ fc-practice/src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.example.logic.BubbleSort;
+import org.example.logic.Sort;
 
 import java.util.Arrays;
 
@@ -8,7 +9,11 @@ import java.util.Arrays;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        BubbleSort<String> sort = new BubbleSort<>();
+        // 해당 메인클래스가 버블정렬에 강결합되어 있는 상태
+        // 즉, 메인은 현재 정렬하기 하기 위한 방법을 알고 있는 상태
+        // BubbleSort<String> sort = new BubbleSort<>();
+
+        Sort<String> sort = new BubbleSort<>();
 
         System.out.println("[result] " + sort.sort(Arrays.asList(args)));
 
